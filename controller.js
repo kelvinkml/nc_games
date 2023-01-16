ceaad5b0007abf435aea97f5b75a8fc49c75ce3a
kelvinkml <kelvinkml@live.com>
2023-01-16 16:35:06 +0000

    removed console logs

--- controller.js
+++ controller.js
@@ -4,16 +4,14 @@ const {fetchCategories, fetchReviews} = require('./model')
 const getCategories = (request, response, next) => {
     fetchCategories()
         .then((categories)=>{
-        response.status(200).send(categories.rows)
+            response.status(200).send(categories.rows)
         })
         .catch(next)   
 }
 
 const getReviews = (request, response, next) => {
-    fetchReviews()
-        
+    fetchReviews()       
         .then((reviews)=>{
-            // console.log(reviews.rows)
             response.status(200).send(reviews.rows)
         }).catch(next)
 }
